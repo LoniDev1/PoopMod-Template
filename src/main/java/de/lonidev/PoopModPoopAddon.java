0fba9f013846c6a1a851d3c50869f97574151fdd
BuildTools <unconfigured@null.spigotmc.org>
2025-04-09 16:54:22 +0200

    Merge remote changes

---
+++ src/main/java/de/lonidev/PoopModPoopAddon.java
@@ -0,0 +1,34 @@
+package de.lonidev;
+
+import de.lonidev.item.Moditems;
+import net.fabricmc.api.ModInitializer;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PoopModPoopAddon implements ModInitializer {
+	public static final String MOD_ID = "poop-mod-poop-addon";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		//Register your Items...
+		Moditems.registerModItems();
+
+
+		//That's how you add Items to the existing Poopmod Item Group
+		RegistryKey<ItemGroup> PoopModItemGroup = RegistryKey.of(Registries.ITEM_GROUP.getKey(), new Identifier("poopmod", "poop_mod"));
+
+		ItemGroupEvents.modifyEntriesEvent(PoopModItemGroup).register(entries -> {
+			//Here are you're items
+			entries.add(Moditems.STINKYPOOP);
+		});
+	}
+}
---
+++ src/main/java/de/lonidev/PoopModPoopAddonDataGenerator.java
@@ -0,0 +1,11 @@
+package de.lonidev;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PoopModPoopAddonDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
