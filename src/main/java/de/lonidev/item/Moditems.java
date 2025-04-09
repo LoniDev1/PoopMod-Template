a6d4eb748c65525338e3b880d5279d17b5469b9a
BuildTools <unconfigured@null.spigotmc.org>
2025-04-09 16:55:25 +0200

    Committing local changes

---
+++ src/main/java/de/lonidev/item/Moditems.java
@@ -0,0 +1,31 @@
+package de.lonidev.item;
+
+
+import de.lonidev.PoopModPoopAddon;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class Moditems {
+    public static final Item STINKYPOOP =  registerItem("stinky_poop", new Item(new FabricItemSettings()));
+
+    private static void addItemsToIngedientTabItemGroup(FabricItemGroupEntries entries) {
+    }
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(PoopModPoopAddon.MOD_ID, name), item);
+    }
+
+
+    public static void registerModItems() {
+        PoopModPoopAddon.LOGGER.info("Registering Mod Items for " + PoopModPoopAddon.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(Moditems::addItemsToIngedientTabItemGroup);
+    }
+}
